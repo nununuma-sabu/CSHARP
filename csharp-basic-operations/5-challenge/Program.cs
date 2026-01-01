1228725ddffbb780ff884ecc9c9dbf2190669580
nununuma-sabu <niigata.info.kt@gmail.com>
2026-01-01 16:46:13 +0900

    不要なディレクトリ削除

--- csharp-basic-operations/5-challenge/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿int fahrenheit = 94;
-decimal celsius = ((decimal)fahrenheit - 32) * 5 / 9; // fahrenheitがint型なので、decimalにキャストしてから計算する
-Console.WriteLine($"The temperature in Celsius is: {celsius} Celsius");
--- csharp-write-first/4-challenge/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
